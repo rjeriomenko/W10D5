c5e0d5aff167d9dd7aec030887c1048f188e111c
rokasj <rokas@jerio.me>
2023-05-12 15:07:21 -0400

    Add project files

---
+++ art-museum/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import harvardArt from './data/harvardArt.js';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
